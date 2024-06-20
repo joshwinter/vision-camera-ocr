66f3e47d6ec5f604c05777076b2c69d241582b1d
FClementi <francesco.clementi1992@gmail.com>
2024-06-20 17:01:47 +0200

    Upgrade react native version example

--- example/ios/VisionCameraOcrExampleTests/VisionCameraOcrExampleTests.m
+++ example/ios/VisionCameraOcrExampleTests/VisionCameraOcrExampleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface VisionCameraOcrExampleTests : XCTestCase
+@interface visioncameraocrexampleTests : XCTestCase
 
 @end
 
-@implementation VisionCameraOcrExampleTests
+@implementation visioncameraocrexampleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
